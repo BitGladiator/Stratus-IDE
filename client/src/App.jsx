1f6d65df7e9c0232360fa447853098dceeb478bc
Karan Sharma <karansh1232@gmail.com>
2025-08-26 00:08:03 +0530

    Improved terminal logic

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Terminal from './components/terminal'
+function App() {
+  return (
+    <div className='playground-container'>
+      <div className='editor-container'></div>
+      <div className='terminal-container'>
+        <Terminal/>
+      </div>
+    </div>
+  )
+}
+
+export default App
