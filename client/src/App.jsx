ae19fb699096d6f6cf4f9eb7964699e0cf50bd49
Karan Sharma <karansh1232@gmail.com>
2025-08-27 11:32:24 +0530

    Completed basic terminal,editor and file tree logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,98 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Terminal from './components/terminal'
-import FileTree from './components/tree'
-import socket from './socket'
-import AceEditor from 'react-ace';
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import Terminal from "./components/terminal";
+import FileTree from "./components/tree";
+import socket from "./socket";
+import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/src-noconflict/ext-language_tools";
 
 function App() {
-  const [fileTree, setFileTree] = useState({})
-  const getFileTree = async() =>{
-    const response = await fetch("http://localhost:9000/files")
-    const result = await response.json(); 
+  const [fileTree, setFileTree] = useState({});
+  const [selectedFile, setSelectedFile] = useState("");
+  const [code, setCode] = useState("");
+  const [selectedFileContent, setSelectedFileContent] = useState("");
+  const isSaved = selectedFileContent === code;
+  const getFileTree = async () => {
+    const response = await fetch("http://localhost:9000/files");
+    const result = await response.json();
     setFileTree(result.tree);
-  }
-  useEffect(()=>{
+  };
+  const getFileContents = useCallback(async () => {
+    if (!selectedFile) {
+      return;
+    }
+    const response = await fetch(
+      `http://localhost:9000/files/content?path=${selectedFile}`
+    );
+    const result = await response.json();
+    setSelectedFileContent(result.content);
+  }, [selectedFile]);
+
+  useEffect(() => {
+    if (selectedFile) {
+      getFileContents();
+    }
+  }, [getFileContents, selectedFile]);
+  useEffect(() => {
     getFileTree();
-  },[])
-  useEffect(()=>{
-   socket.on('file:refresh',getFileTree);
-   return ()=>{
-    socket.off('file:refresh',getFileTree)
-   }
-  },[])
-  return ( 
-    <div className='playground-container'>
-      <div className='editor-container'>
-         <div className="files">
-          <FileTree tree={fileTree}/>
-         </div>
-         <div className="editor"><AceEditor/></div>
+  }, []);
+  useEffect(() => {
+    socket.on("file:refresh", getFileTree);
+    return () => {
+      socket.off("file:refresh", getFileTree);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (code && !isSaved) {
+      const timer = setTimeout(() => {
+        socket.emit("file:change", {
+          path: selectedFile,
+          content: code,
+        });
+      }, 5 * 1000);
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [code, selectedFile, isSaved]);
+  // useEffect(()=>{
+  //   setCode("");
+  // },[selectedFile])
+  useEffect(() => {
+    if (selectedFile && selectedFileContent) {
+      setCode(selectedFileContent);
+    }
+  }, [selectedFile, selectedFileContent]);
+
+  return (
+    <div className="playground-container">
+      <div className="editor-container">
+        <div className="files">
+          <FileTree
+            onSelect={(path) => {
+              if (path !== selectedFile) {
+                setSelectedFile(path);
+                setSelectedFileContent("");
+                setCode("");
+              }
+            }}
+            tree={fileTree}
+          />
+        </div>
+        <div className="editor">
+          {selectedFile && <p>{selectedFile.replaceAll("/", " > ")} {isSaved?"Saved":"Unsaved"}</p>}
+
+          <AceEditor value={code} onChange={(e) => setCode(e)} />
+        </div>
       </div>
-      <div className='terminal-container'>
-        <Terminal/>
+      <div className="terminal-container">
+        <Terminal />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/tree.jsx
+++ client/src/components/tree.jsx
@@ -1,19 +1,26 @@
-const FileTreeNode = ({ fileName, nodes,onSelect,path}) => {
-  const  isDir = !!nodes
+
+const FileTreeNode = ({ fileName, nodes, onSelect, path }) => {
+  const isDir = !!nodes;
   return (
-    <div onClick={(e)=>{
-       e.stopPropagation()
-       if(!isDir){
-        return;
-       }
-       onSelect(path)
-    }} style={{marginLeft: "10px"}}>
-      <p className={isDir ? "":"file-node"}>{fileName}</p>
-      {nodes && (
+    <div
+      onClick={(e) => {
+        e.stopPropagation();
+        if (isDir) return;
+        onSelect(path);
+      }}
+      style={{ marginLeft: "10px" }}
+    >
+      <p className={isDir ? "" : "file-node"}>{fileName}</p>
+      {nodes && fileName !== "node_modules" && (
         <ul>
           {Object.keys(nodes).map((child) => (
             <li key={child}>
-              <FileTreeNode fileName={child} path={path+"/"+child } nodes={nodes[child]} />
+              <FileTreeNode
+                onSelect={onSelect}
+                path={path + "/" + child}
+                fileName={child}
+                nodes={nodes[child]}
+              />
             </li>
           ))}
         </ul>
@@ -22,8 +29,7 @@ const FileTreeNode = ({ fileName, nodes,onSelect,path}) => {
   );
 };
 
-const FileTree = ({ tree }) => {
-  console.log(tree);
-  return <FileTreeNode fileName="/" path=""  nodes={tree} />;
+const FileTree = ({ tree, onSelect }) => {
+  return <FileTreeNode onSelect={onSelect} fileName="/" path="" nodes={tree} />;
 };
 export default FileTree;
